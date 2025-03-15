7b835b80a157b7187a148211a4cf0d57c64ae57b
sylviavitner <vitnersylvia@gmail.com>
2025-03-15 15:02:42 -0400

    get block image

--- block.py
+++ block.py
@@ -15,8 +15,16 @@ class Block:
         }
 
         # Call a function to get the specified block on the sprite sheet
+        self.sprite_sheet = pygame.image.load("dirt-temp.png") # temp sprite sheet
         self.image = self.get_block(block_type)
 
     # Get the specified block from the sprite sheet
-    def get_block(block_type):
-        return
+    def get_block(self, block_type):
+        # Handle non block-type errors
+        if block_type not in self.block_dict:
+            raise ValueError(f"Unknown block type: {block_type}")
+        
+        x, y = self.block_dict[block_type] # Get the position of the block in the sprite sheet
+
+        # Return the specified block
+        return self.sprite_sheet.subsurface(pygame.Rect(x, y, self.width, self.height))
